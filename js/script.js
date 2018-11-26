8dd9d68fb54f1976d685549f54217461fb5d58c2
Zeyd Alabsi <gotozeyd336@gmail.com>
2018-11-26 23:05:08 +0000

    third edit

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+var userChoice;
+
+var computerChoice;
+
+var winner;
+
+var randomNumber = 0;
+
+$("#shoot").click(function(){
+   userChoice = $("#input").val(); 
+    randomNumber = Math.random();  
+   
+   if (randomNumber < .33 ) {
+      computerChoice = "rock"
+     // else if (randomNumber < .66)
+         
+      
+   }
+   computerChoice = $("#input").val(); 
+   
+   
+   winner = $("#input").val(); 
+   
+});
